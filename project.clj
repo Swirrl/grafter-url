703e0a52bafb644775cdbf3f69b5329ed8872b5f
Rick Moynihan <rick.m@swirrl.com>
2015-04-13 11:59:28 +0100

    Fix description & project name.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject grafter-url "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject grafter/url "0.1.0-SNAPSHOT"
+  :description "A standalone library to build and manipulate URLs and
+  their component parts."
+  :url "http://grafter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
